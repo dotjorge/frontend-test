8d04810bf75f207e581f2ea57526229b8849fac3
dotjorge <jorgeluiz_dj2010@hotmail.com>
2022-12-31 20:51:15 -0300

    test: Criado test pra função getPercent

---
+++ components/Ranking/functions/getPercent/getPercent.test.js
@@ -0,0 +1,22 @@
+import "@testing-library/jest-dom";
+import { getPercent } from "../getPercent/getPercent";
+
+describe("Home", () => {
+  it("Deve retornar um objeto com as porcentagens corretas", () => {
+    const percent = getPercent(50, 50);
+
+    expect(percent).toMatchObject({
+      positive: 50,
+      negative: 50,
+    });
+  });
+
+  it("Deve retornar 0 se os valores forem 0", () => {
+    const percent = getPercent(0, 0);
+
+    expect(percent).toMatchObject({
+      positive: 0,
+      negative: 0,
+    });
+  });
+});
