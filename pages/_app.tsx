74e329a2b9673607a7263807bed1313eee0b6230
dotjorge <jorgeluiz_dj2010@hotmail.com>
2022-12-31 10:02:37 -0300

    style: Adicionado css global

---
+++ pages/_app.tsx
@@ -0,0 +1,8 @@
+import "../globalstyles.css";
+import type { AppProps } from "next/app";
+
+const App = ({ Component, pageProps }: AppProps) => {
+  return <Component {...pageProps} />;
+};
+
+export default App;
