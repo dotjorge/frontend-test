cec734788c6f0c81330faad4fdbb8c2698a11ed3
dotjorge <jorgeluiz_dj2010@hotmail.com>
2022-12-31 09:41:14 -0300

    type: Corrigido interfaces fazenda

--- types/fazenda.ts
+++ types/fazenda.ts
@@ -1,3 +1,18 @@
-import fazenda from "mocks/fazenda.json";
+export interface Fazenda {
+  version: string;
+  box_name: string;
+  data: FazendaDataItem[];
+}
 
-export type Fazenda = typeof fazenda;
+export type FazendaData = FazendaDataItem[];
+
+export interface FazendaDataItem {
+  __id: string;
+  timestamp: number;
+  name: string;
+  description: string;
+  picture: string;
+  positive?: number | string;
+  negative?: number | string;
+  age?: number;
+}
--- types/index.ts
+++ types/index.ts
@@ -1,4 +1,4 @@
-export { type Fazenda } from "./fazenda";
+export { type Fazenda, type FazendaDataItem } from "./fazenda";
 export { type FC } from "./fc";
 
 // export interface Types {
