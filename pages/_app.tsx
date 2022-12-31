8852e34e13b4dbb8d02df9e03c712794ad8c19ca
dotjorge <jorgeluiz_dj2010@hotmail.com>
2022-12-31 13:41:56 -0300

    seo: Adicionado meta tags no _app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,19 @@
 import "../globalstyles.css";
 import type { AppProps } from "next/app";
+import Head from "next/head";
 
 const App = ({ Component, pageProps }: AppProps) => {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <html lang="pt-br" />
+        <title>A Fazenda - R7</title>
+        <meta property="og:title" content="A Fazenda - R7" key="title" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 };
 
 export default App;
