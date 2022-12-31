f8b2fef592ad004a59fd562d232329f80e23d018
dotjorge <jorgeluiz_dj2010@hotmail.com>
2022-12-31 13:44:48 -0300

    feat: Usando porcentagem getPercent no Item

--- components/Ranking/components/Item/Item.tsx
+++ components/Ranking/components/Item/Item.tsx
@@ -1,18 +1,39 @@
 import Styled from "./Item.styles";
 import Image from "next/image";
 import { FC } from "types";
-import { IItem } from "./Item.types";
+import { IRankingItem } from "./Item.types";
+import { getPercent } from "../functions";
 
-export const Item = (props: FC<IItem>) => {
+export const Item = (props: FC<IRankingItem>) => {
+  const { positive, negative, position } = props;
+
+  const percentage = getPercent(positive, negative);
+
   return (
     <Styled.Item className="ranking__item">
       <div className="ranking__item__photo">
         <Image src={props.picture} alt={props.name} fill />
-        <span className="ranking__item__photo__bullet">1</span>
+        <span className="ranking__item__photo__bullet">{position + 1}</span>
       </div>
       <div className="ranking__item__info">
         <p className="ranking__item__info__name">{props.name}</p>
         <p className="ranking__item__info__description">{props.description}</p>
+      </div>
+      <div className="ranking__item__tooltip">
+        <div className="ranking__item__tooltip__reputation">
+          <div className="ranking__item__tooltip__reputation_title">Gostam</div>
+          <div className="ranking__item__tooltip__reputation__likes">
+            {`${percentage.positive}%`}
+          </div>
+        </div>
+        <div className="ranking__item__tooltip__reputation">
+          <div className="ranking__item__tooltip__reputation_title">
+            Não gostam
+          </div>
+          <div className="ranking__item__tooltip__reputation__likes">
+            {`${percentage.negative}%`}
+          </div>
+        </div>
       </div>
     </Styled.Item>
   );
