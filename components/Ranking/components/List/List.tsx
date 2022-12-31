4794e15be67d9fda6a2a68299a6ca9492266c8e5
dotjorge <jorgeluiz_dj2010@hotmail.com>
2022-12-31 13:46:31 -0300

    feat: Usando lista ordenada por maior porcentagem de positive na List

--- components/Ranking/components/List/List.tsx
+++ components/Ranking/components/List/List.tsx
@@ -2,18 +2,28 @@ import Styled from "./List.styles";
 import { FC } from "types";
 import { Item } from "../";
 import { IList } from "./List.types";
+import { getPercent } from "../functions";
 
 export const List = (props: FC<IList>) => {
+  const { data } = props;
+
+  const orderedList = [...data].sort((a, b) => {
+    const currentItem = getPercent(a.positive, a.negative);
+    const acc = getPercent(b.positive, b.negative);
+    return acc.positive - currentItem.positive;
+  });
+
   return (
     <Styled.List className="ranking">
-      {props.data?.map((item) => (
+      {orderedList?.map((item, index) => (
         <Item
           key={item.id}
+          position={index}
           id={item.id}
           picture={item.picture}
           name={item.name}
           description={item.description}
-          positive={item.negative}
+          positive={item.positive}
           negative={item.negative}
         />
       ))}
