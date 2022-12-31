70a43d6202906e171884ece77e92a3674c02e66a
dotjorge <jorgeluiz_dj2010@hotmail.com>
2022-12-31 17:49:06 -0300

    style: Adicionado fonte Montserrat do next/font no _app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,13 @@
 import "../globalstyles.css";
+import { Montserrat } from "@next/font/google";
 import type { AppProps } from "next/app";
 import Head from "next/head";
+
+const montserrat = Montserrat({
+  weight: ["400", "500", "600", "700", "800", "900"],
+  style: ["normal", "italic"],
+  subsets: ["latin"],
+});
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
@@ -9,7 +16,9 @@ const App = ({ Component, pageProps }: AppProps) => {
         <html lang="pt-br" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
-      <Component {...pageProps} />
+      <main className={montserrat.className}>
+        <Component {...pageProps} />
+      </main>
     </>
   );
 };
