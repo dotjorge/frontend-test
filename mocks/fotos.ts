87d6c6b91feeeac2aa0132e108f9e457d40a37da
dotjorge <jorgeluiz_dj2010@hotmail.com>
2022-12-31 09:41:53 -0300

    setup: Adicionado mock de fotos locais (as do url não funcionam)

---
+++ mocks/fotos.ts
@@ -0,0 +1,13 @@
+import AndressaUrach from "/public/andressa-urach.png";
+import BarbaraEvans from "/public/barbara-evans.png";
+import Gominho from "/public/gominho.png";
+import RitaCadillac from "/public/rita-cadillac.png";
+import YudiTamashiro from "/public/yudi-tamashiro.png";
+
+export const fotos = {
+  AndressaUrach,
+  BarbaraEvans,
+  Gominho,
+  RitaCadillac,
+  YudiTamashiro,
+};
